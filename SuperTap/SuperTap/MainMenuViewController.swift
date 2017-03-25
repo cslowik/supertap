6b9b45cb445eaa3b4ae1e6de68de1a7de542ef9d
Chris Slowik <chris@creativeda.sh>
2017-03-25 11:40:35 -0700

    started project. made home screen.

---
+++ SuperTap/SuperTap/MainMenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainMenuVC.swift
+//  SuperTap
+//
+//  Created by Chris Slowik on 3/25/17.
+//  Copyright © 2017 Chris Slowik. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
